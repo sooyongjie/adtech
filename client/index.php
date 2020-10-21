6f919e7d97060d45a92880c742a477135d9862c8
Soo Yong Jie <sooyongjie@gmail.com>
2020-10-21 23:32:10 +0800

    Added SLICK ANIMATIONS to client landing page

--- client/index.php
+++ client/index.php
@@ -12,6 +12,9 @@
     <link rel="stylesheet" type="text/css" href="../css/style.css" />
     <link rel="stylesheet" type="text/css" href="../css/client/client.css" />
     <link rel="stylesheet" type="text/css" href="../css/client/client-landing-page.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"
+        integrity="sha512-IQLehpLoVS4fNzl7IfH8Iowfm5+RiMGtHykgZJl9AWMgqx0AmJ6cRWcB+GaGVtIsnC4voMfm8f2vwtY+6oPjpQ=="
+        crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -29,4 +32,10 @@
     </div>
 </body>
 
+<script>
+    var gsap = gsap.timeline();
+    gsap.to(".button", {border: "1px solid #cccccc", duration: 0.1, stagger: 0.05}); //wait 2 seconds
+    gsap.to("i", {opacity: 1, duration: 0.2, delay: 0}); //wait 2 seconds
+    gsap.to("p", {opacity: 1, duration: 0.2}, "-=0.2"); //wait 2 seconds
+</script>
 </html>
