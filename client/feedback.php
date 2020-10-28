1f3959a0ccdd8b23024fc7bd293f43d670f1ab2b
Alicia <aliciacheong99@gmail.com>
2020-10-28 23:40:29 +0800

    commit change

--- client/feedback.php
+++ client/feedback.php
@@ -13,25 +13,26 @@
     <link rel="stylesheet" type="text/css" href="../css/style.css" />
     <link rel="stylesheet" type="text/css" href="../css/client/client.css" />
     <link rel="stylesheet" type="text/css" href="../css/client/client-new-request.css" />
+    <link rel="stylesheet" type="text/css" href="../css/client/feedback.css" />
 </head>
 
 <body>
     <?php include_once("nav-back-button.php") ?>
-    <div class="container">
+    <div class="container" onclick="setRating(2)">
         <form action=".php" method="post">
             <!-- <div class="form-header" onclick="location.href='client.html'">
                 <i class="fas fa-arrow-left fa-md"></i>
                 <h2>Request Form</h2>
             </div> -->
             <h2>Feedback For Request <?php echo "#".$_POST['reqID'] ?></h2>
-            <label for="">Category</label>
-            <div class="radio">
-                <input type="radio" name= "select" value="Hardware">
-                <span>Hardware</span>
-                <input type="radio" name= "select" value="Software">
-                <span>Software</span>
-                <input type="radio" name= "select" value="Others">
-                <span>Others</span>
+            <label for="">Service Request Rating</label>
+            <div class="star-widget">
+            <input type="hidden" name="rate" value="5" id="rating">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
             </div>
             <label for="">Feedback Comment</label>
             <input type="text" name="textinput" placeholder="Enter comment">
@@ -39,5 +40,11 @@
         </form>
     </div>
 </body>
-
 </html>
+
+<script>
+        function setRating(num){
+        const inputElement = document.querySelector('#rating')
+        inputElement.value = num
+    }
+</script>
