8c5d9edbc5f6283aa6f377282d0f75e1d75d39c0
Soo Yong Jie <sooyongjie@gmail.com>
2020-10-29 16:16:32 +0800

    SUPERNOVA 🌟

--- client/feedback.php
+++ client/feedback.php
@@ -25,14 +25,16 @@
                 <h2>Request Form</h2>
             </div> -->
             <h2>Feedback For Request <?php echo "#".$_POST['reqID'] ?></h2>
+            <label for="">Payment</label>
+            <input type="file">
             <label for="">Service Request Rating</label>
             <div class="star-widget">
-            <input type="hidden" name="rate" value="0" id="rating">
-            <i id="1" class="fas fa-star" onclick="setRating(1)" onmouseover="hover(1)"></i>
-            <i id="2" class="fas fa-star" onclick="setRating(2)" onmouseover="hover(2)"></i>
-            <i id="3" class="fas fa-star" onclick="setRating(3)" onmouseover="hover(3)"></i>
-            <i id="4" class="fas fa-star" onclick="setRating(4)" onmouseover="hover(4)"></i>
-            <i id="5" class="fas fa-star" onclick="setRating(5)" onmouseover="hover(5)"></i>
+                <input type="hidden" name="rate" value="0" id="rating">
+                <i id="1" class="fas fa-star" onclick="setRating(1)" onmouseover="hover(1)"></i>
+                <i id="2" class="fas fa-star" onclick="setRating(2)" onmouseover="hover(2)"></i>
+                <i id="3" class="fas fa-star" onclick="setRating(3)" onmouseover="hover(3)"></i>
+                <i id="4" class="fas fa-star" onclick="setRating(4)" onmouseover="hover(4)"></i>
+                <i id="5" class="fas fa-star" onclick="setRating(5)" onmouseover="hover(5)"></i>
             </div>
             <label for="">Feedback Comment</label>
             <input type="text" name="textinput" placeholder="Enter comment">
--- client/js/feedback.js
+++ client/js/feedback.js
@@ -1,8 +1,11 @@
 function hover(num) {
     let stars = document.querySelectorAll('.fa-star')
     console.log('stars: ', stars[0].id);
+    for (let i = 0; i < 5; i++) {
+        stars[i].style.color = "#aaaaaa";
+    }
     for (let i = 0; i < num; i++) {
-        stars[i].style.backgroundColor = "red";
+        stars[i].style.color = "yellow";
     }
 }
 
