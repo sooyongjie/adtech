4d3b57e15f9556f64109dab5ee45e750970be5ef
Alicia <aliciacheong99@gmail.com>
2020-10-30 16:15:55 +0800

    Log out

--- client/nav-back-button.php
+++ client/nav-back-button.php
@@ -7,5 +7,8 @@ session_start();
         <span>Back</span>
     </div>
     <p class="user"><?php echo $_SESSION['cName'] ?></p>
+    <div class="logout" onclick="location.href='index.php'">
+    <p><span>Log Out</span></p>
+    </div>
     <h1 onclick="location.href='dashboard.php'">Ad<span>tech</span></h1>
 </nav>
--- client/nav.php
+++ client/nav.php
@@ -3,5 +3,8 @@ session_start();
 ?>
 <nav>
     <p class="user"><?php echo $_SESSION['cName'] ?></p>
+    <div class="logout" onclick="location.href='index.php'">
+    <p><span>Log Out</span></p>
+    </div>
     <h1 onclick="location.href='dashboard.php'">Ad<span>tech</span></h1>
 </nav>
